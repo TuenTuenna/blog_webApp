7f0a6145cc3df3636b84b2ad0144afd50be9340e
kor216465 <kor216465@gmail.com>
2019-06-19 20:03:22 +0900

    before integrate database

--- blog_app.js
+++ blog_app.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 
 const ejs = require("ejs");
 
+// Load the full build.
+// 로다쉬 임포트
+// 참조: https://lodash.com/
+const _ = require('lodash');
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -73,6 +78,35 @@ app.get("/compose", function(req, res){
   // ejs 는 views 폴더 안에 있는 파일명으로 랜더링한다.
   res.render("compose");
 });
+
+/*
+  Post Route
+*/
+// 참조: http://expressjs.com/en/guide/routing.html
+app.get("/posts/:postId", function (req, res) {
+// 참조: https://lodash.com/docs/4.17.11#lowerCase
+  const requestedTitle = _.lowerCase(req.params.postId);
+  // res.send(req.params)
+  // console.log(req.params.postId);
+  // url 이 포스트 배열에 있으면 알려준다.
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+      // console.log("req.params.postId : " + requestedTitle);
+      // console.log("post.title : " + storedTitle);
+      if(storedTitle === requestedTitle){
+          // console.log("Match found!");
+          // post.ejs 파일을 열고 값을 보내준다.
+          // <%= post_title %> <%= post_content %>
+          res.render("post", {post_title: post.title, post_content: post.content});
+      }
+  });
+
+
+});
+
+// app.post("/posts/:postId", function(req, res){
+//
+// });
 
 
 
